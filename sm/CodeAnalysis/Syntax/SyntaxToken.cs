bc84c13e23c4d965c0575f81b662d9990a5254cc
chenyekun1 <1181510942@qq.com>
2019-03-23 21:14:40 +0800

    Fix unknown bug.

--- sm/CodeAnalysis/Syntax/SyntaxToken.cs
+++ sm/CodeAnalysis/Syntax/SyntaxToken.cs
@@ -20,7 +20,7 @@ namespace mc.CodeAlalysis.Syntax
         public string Text { get; }
         public object Value { get; }
 
-        public override TextSpan Span => new TextSpan(Position, Text.Length);
+        public override TextSpan Span => new TextSpan(Position, Text?.Length ?? 0);
 
         public override IEnumerable<SyntaxNode> GetChildren()
         {
